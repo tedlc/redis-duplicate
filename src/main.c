a5cc51752341b9cda5b2f016324aa786a733f0ac
Zhiyu Yuan <yuanzhiy218@gmail.com>
2025-03-01 22:55:17 -0500

    Allow I/O multiplexing with event loop

--- src/main.c
+++ src/main.c
@@ -6,28 +6,51 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <poll.h>
+
+// wait indefinitely
+#define SERVER_FD_POLL_TIMEOUT -1
+#define BUFFER_SIZE 1096
+
+const nfds_t SERVER_FD_POLL_NUMBER = 256;
+
+void read_handler(int client_fd, int read_size, struct pollfd fds[], int fds_length) {
+    char buffer[BUFFER_SIZE];
+    int err = read(client_fd, buffer, 15);
+    // client has been disconnected
+    if (err <= 0) {
+        close(client_fd);
+        // deregister the client fd from the subsciption list
+        for (int i = 0; i < fds_length; i++) {
+            if (fds[i].fd == client_fd) {
+                fds[i].fd = -1;
+                break;
+            }
+        } 
+    }
+    write(client_fd, "+PONG\r\n", 7);
+    return;
+}
 
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
 	
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	// Uncomment this block to pass the first stage
-	//
-	socklen_t server_fd, client_addr_len;
+	socklen_t client_addr_len;
+    int server_fd;
 	struct sockaddr_in client_addr;
-	
+
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Server socket is created, waiting...\n");
+
 	if (server_fd == -1) {
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return 1;
 	}
 
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
@@ -44,28 +67,60 @@ int main() {
 		return 1;
 	}
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
+    printf("Server socket bind to port 6379. \n");
 
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        printf("Listen failed: %s \n", strerror(errno));
+        return 1;
+    }
 
-	int client_fd;
-    client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
-    char buffer[256]; 
-    bool flag = false;
+    // fds to track, fds[0] should be the listening socket for accepting new TCP connection
+    struct pollfd fds[256];
+    fds[0].fd = server_fd;
+    fds[0].events = POLLIN;
+    int fds_length = sizeof(fds) / sizeof(fds[0]);
+
+    for (int i = 1; i < fds_length; i++) {
+        fds[i].fd = -1;
+    }
+    
+    // Main event loop
     while (1) {
-        int err = read(client_fd, buffer, 15);
-        if (err <= 0) {
-            close(client_fd);
+        // listen for new client connection request
+        int ret = poll(fds, SERVER_FD_POLL_NUMBER, SERVER_FD_POLL_TIMEOUT);
+        if (ret < 0) {
+            printf("Poll failed.\n");
+            close(server_fd);
             return 0;
         }
-        write(client_fd, "+PONG\r\n", 7);
+        printf("Calling poll...\n");
+
+        // ready to accept new connections
+        if (ret > 0) {
+            if ((fds[0].revents & POLLIN) == POLLIN) {
+                client_addr_len = sizeof(client_addr);
+                int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+                printf("Client connected.\n");
+                for (int i = 1; i < fds_length; i++) {
+                    if (fds[i].fd == -1) {
+                        fds[i].fd = client_fd;
+                        fds[i].events = POLLIN;
+                        break;
+                    }
+                }
+            }
+
+            // Read event
+            for (int i = 1; i < fds_length; i++) {
+                if ((fds[i].revents & POLLIN) == POLLIN) {
+                    // handover to the handler 
+                    read_handler(fds[i].fd, 15, fds, fds_length);
+                }
+            }
+        }
     }
+
 	close(server_fd);
 
 	return 0;
